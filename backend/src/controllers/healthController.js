cf986af875d49baf0497e1c7ced16bbe0a459621
SamuelCFalcao <samuelfalcao@alu.ufc.br>
2025-10-31 16:19:06 -0300

    Init commit estrutura do projeto criada

--- backend/src/controllers/healthController.js
+++
@@ -1,5 +0,0 @@
-export function health(req, res) {
-  res.json({ status: 'OK', message: 'API funcionando corretamente' })
-}
-
-
--- backend/src/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { listProducts, getProduct } from "../controllers/productController.js";
-
-const router = Router();
-
-router.get("/", listProducts);
-router.get("/:id", getProduct);
-
-export default router;
